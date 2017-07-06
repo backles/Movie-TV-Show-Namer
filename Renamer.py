6091ac4bad8e103242fc9baa5956335cad45af00
BLPerry <backles1994@gmail.com>
2017-07-06 13:23:45 -0700

    added regular expressions
    added regular expressions,
    and moved functions around

--- Renamer.py
+++ Renamer.py
@@ -4,34 +4,56 @@ import glob
 import os
 import string
 import shutil
-
-tVShowDirectory = 'D:\TransferTV'
-movieDirectory = 'D:\Transfer'
-readDirectory = 'C:\\Users\\Admin\\Desktop'
-TVString = 'S**E**'
-files = os.listdir(readDirectory)
-
-#print(files)
+from xml.dom import minidom
 
 
-
-
-def moveMovie(fileName):
+def moveMovie(readDirectory, movieDirectory, fileName):
 	print("Is Movie")
 	shutil.move(readDirectory + fileName, movieDirectory + fileName)
 
 def tvShow():
 	print("Is TVshow")
-
-def main():
+	
+def getAllMP4(readDirectory, movieDirectory, tVsDirectory, files):
 	for file in files:
+		originalFile = file
 		if file.endswith(".mp4"):
-			file = file[:-4]				#removes file extension from list
-			fileStringName = file.split()	#splits the string to look for movie or TV show	
-			print (fileStringName)			#print the list		
-			if re.match("(^[A-Z]\d[A-Z]\d", fileStringName[-1]):
-				print("It worked")
-			else: #Movie
-				moveMovie(file)
+			file = file[:-4]					#removes file extension from list
+			if '.' in file:						#If there are any periods in the title minus the extension
+				file = file.replace(".", " ")	#remove periods
+			fileStringName = file.split()		#splits the string to look for movie or TV show	
+			length = len(fileStringName)
+			if re.match("(^[A-Z][0-9][0-9][A-Z][0-9][0-9])", fileStringName[length-1]):
+				tvShow(readDirectory, tVsDirectory, originalFile)
+			elif re.match("(^[A-Z][0-9][A-Z][0-9])", fileStringName[length-1]):
+				tvShow(readDirectory, tVsDirectory, originalFile)
+			elif re.match("(^[A-Z][0-9][0-9][A-Z][0-9])", fileStringName[length-1]):
+				tvShow(readDirectory, tVsDirectory, originalFile)
+			elif re.match("(^[A-Z][0-9][A-Z][0-9][0-9])", fileStringName[length-1]):
+				tvShow(readDirectory, tVsDirectory, originalFile)
+			elif re.match("(^[a-z][0-9][0-9][a-z][0-9][0-9])", fileStringName[length-1]):
+				tvShow(readDirectory, tVsDirectory, originalFile)
+			elif re.match("(^[a-z][0-9][a-z][0-9])", fileStringName[length-1]):
+				tvShow(readDirectory, tVsDirectory, originalFile)
+			elif re.match("(^[a-z][0-9][0-9][a-z][0-9])", fileStringName[length-1]):				
+				tvShow(readDirectory, tVsDirectory, originalFile)
+			elif re.match("(^[A-Z][0-9][A-Z][0-9][0-9])", fileStringName[length-1]):
+				tvShow(readDirectory, tVsDirectory, originalFile)
+			else: 																			#Else if Movie
+				moveMovie(readDirectory, moveMovie, originalFile)
+
+def getDirectoriesandSettings():
+	xmldoc = minidom.parse('settings.xml')
+	itemlist = xmldoc.getElementsByTagName('item')
+	readDirectory = itemlist[0].attributes['locationToMoveFrom'].value
+	movieDirectory = itemlist[1].attributes['locationToMoveMoviesTo'].value
+	tVsDirectory = itemlist[2].attributes['locationToMoveTVTo'].value
+	files = os.listdir(readDirectory)
+	return readDirectory, movieDirectory, tVsDirectory, files
+	
+def main():
+	rDirectory, mDirectory, tDirectory, files = getDirectoriesandSettings()
+	getAllMP4(rDirectory, mDirectory, tDirectory, files)
+	
 
 main()
